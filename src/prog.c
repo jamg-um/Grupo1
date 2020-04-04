760e83495bc6505a6c3772a28e6684e616d40d67
Carlos Pontón Valladares <carlosdaniel.pontonv@um.es>
2020-04-04 17:48:31 +0200

    Funciones añadidas

--- src/prog.c
+++ src/prog.c
@@ -7,6 +7,28 @@ int es_par(int num){
 		return 0;
 }
 
+int fibonacci(int n){
+	if (n < 2)
+		return fibonacci(n-1)+fibonacci(n-2);
+	else if (n == 2 || n == 1)
+		return 1;
+	else if (n == 0)
+		return 0;
+	return 0;
+}
+
+int mayor(int a, int b, int c) {
+	int mayor;
+
+	if (a >= b)
+		mayor = a;
+	else mayor = b;
+
+	if (mayor >= c)
+		return mayor;
+	else return c;
+}
+
 int main(void){
 	printf("¿Es 32 un número par?\n");
 	if(es_par(32))
